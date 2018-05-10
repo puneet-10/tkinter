368d9d560ad2c4bace859daec71ecf0d1dd3ce89
Puneet <agrawal.puneet.10@gmail.com>
2018-05-11 00:19:35 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -14,28 +14,28 @@ def get_selected_row(event):
     e3.insert(END,selected_tuple[3])
     e4.delete(0,END)
     e4.insert(END,selected_tuple[4])
-
+##stting the row event for the desktop application
 def view_command():
     list1.delete(0,END)
     for row in backend.view():
         list1.insert(END,row)
-
+#updating the view option,setting what to do when you click on view
 def search_command():
     list1.delete(0,END)
     for row in backend.search(title_text.get(),Author_text.get(),year_text.get(),ISBN_text.get()):
         list1.insert(END,row)
-
+#same like view updating the delete command
 def add_command():
     backend.insert(title_text.get(),Author_text.get(),year_text.get(),ISBN_text.get())
     list1.delete(0,END)
     list1.insert(END,(title_text.get(),Author_text.get(),year_text.get(),ISBN_text.get()))
-
+#updating the add command
 def delete_command():
     backend.delete(selected_tuple[0])
-
+#updating the delete command
 def update_command():
     backend.update(selected_tuple[0],title_text.get(),Author_text.get(),year_text.get(),ISBN_text.get())
-
+#updating the update command
 window=Tk()
 window.wm_title("BookStore")
 
@@ -50,7 +50,7 @@ l3.grid(row=1,column=0)
 
 l4=Label(window,text="ISBN")
 l4.grid(row=1,column=2)
-
+##putting labels on the commands
 title_text=StringVar()
 e1=Entry(window,textvariable=title_text)
 e1.grid(row=0,column=1)
@@ -62,7 +62,7 @@ e2.grid(row=0,column=3)
 year_text=StringVar()
 e3=Entry(window,textvariable=year_text)
 e3.grid(row=1,column=1)
-
+##updating the text how to show the data from the database
 ISBN_text=StringVar()
 e4=Entry(window,textvariable=ISBN_text)
 e4.grid(row=1,column=3)
@@ -95,6 +95,6 @@ b5.grid(row=6,column=3)
 
 b6=Button(window,text="close",width=12,command=window.destroy)
 b6.grid(row=7,column=3)
-
+##adding the buttons in desktop application for all the given commands or the functions
 
 window.mainloop()
